939e12ab035a30f4ee69867ba3d6ce2bd7f4727e
Marc Bombeck <MBombeck@users.noreply.github.com>
2022-01-02 20:58:45 +0100

    hostname fix

--- win.ps1
+++ win.ps1
@@ -1,2 +1,2 @@
-$postParams = @{hostname='L11PAVA-99009';huid='p29smmemembnsd929299';ip='10.10.10.10';os='Windows 11';version='11.2022';uptime='42'}
+$postParams = @{hostname='L11Test-99009';huid='p29smmemembnsd929299';ip='10.10.10.10';os='Windows 11';version='11.2022';uptime='42'}
 Invoke-WebRequest -Uri http://localhost:3000/pcs -Method POST -Body $postParams
