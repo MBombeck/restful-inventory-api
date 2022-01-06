25decc26d5b81f89f2d28084e46165196f4e1663
Marc Bombeck <MBombeck@users.noreply.github.com>
2022-01-06 17:24:11 +0100

    Update db_setup.sql

--- setup/db_setup.sql
+++ setup/db_setup.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.7
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Erstellungszeit: 06. Jan 2022 um 23:38
--- Server-Version: 5.5.68-MariaDB
--- PHP-Version: 7.3.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -18,15 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Datenbank: `inventory`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `inventory`
---
 
 CREATE TABLE `inventory` (
   `id` int(11) NOT NULL,
@@ -39,31 +21,15 @@ CREATE TABLE `inventory` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Daten für Tabelle `inventory`
---
-
 INSERT INTO `inventory` (`id`, `hostname`, `uuid`, `ip`, `os`, `version`, `uptime`, `updated_at`) VALUES
 (1, 'L11TEST-000001', 'deff0438-0776-4e75-b36d-da6eb2c0946e', '10.14.210.143', 'Windows 11', '10.2000', '120', '2021-12-31 23:00:03');
 
---
--- Indizes der exportierten Tabellen
---
 
---
--- Indizes für die Tabelle `inventory`
---
 ALTER TABLE `inventory`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `hostname` (`hostname`);
 
---
--- AUTO_INCREMENT für exportierte Tabellen
---
 
---
--- AUTO_INCREMENT für Tabelle `inventory`
---
 ALTER TABLE `inventory`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
