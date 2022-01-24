f8dbc837819555d23c00613f6c9f3ee23da7b298
xmochx <97048362+xmochx@users.noreply.github.com>
2022-01-24 08:01:25 +0100

    Create win.ps1
    Added 
    
    cpuname
    cpuload
    ram
    freemem
    logonserver
    loginuser
    vendor
    hardwarename
    biosfirmaretype
    hdd
    hddsize
    hddfree
    externalip
    gateway

--- assets/scripts/win.ps1
+++ assets/scripts/win.ps1
@@ -5,8 +5,8 @@ $url = 'http://localhost:3000/v1/inventory'
 
 # Basic Auth
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
-# IP
-$SourceAddress = (Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring
+# SourceAdress replaced with $IP on line 71
+#$SourceAddress = (Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring
 # OS 
 $os = (Get-WmiObject win32_operatingsystem).caption
 # OS Version
@@ -16,11 +16,42 @@ $uptime = (Get-Date) - (gcim Win32_OperatingSystem).LastBootUpTime | Select-Obje
 $uptime = [math]::Round($uptime)
 # uuid   
 $uuid= (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID
+# cpu name
+$cpuname =(Get-CimInstance -Class Win32_Processor).Name
+# cpu load
+$cpuload = (Get-CimInstance -Class Win32_Processor).LoadPercentage
+# ram total
+$ram = (Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum /1gb
+# free ram
+$freemem = [math]::Round((Get-ciminstance Win32_OperatingSystem).FreePhysicalMemory*1024/1gb,2)
+# logon server
+$logonserver = (Get-ComputerInfo).LogonServer -replace '\\',''
+# logon user
+$loginuser = (Get-ComputerInfo).CsUserName
+# computer vendor
+$vendor = (Get-CimInstance Win32_ComputerSystemProduct).Vendor
+# computer model
+$hardwarename = (Get-CimInstance Win32_ComputerSystemProduct).Name
+# bios type
+$biosfirmaretype = (Get-ComputerInfo).BiosFirmwareType
+# hdd name
+$hdd = (Get-Disk).FriendlyName
+# hdd size
+$hddsize = [math]::Round((Get-Volume -DriveLetter C).Size /1gb)
+# hdd free
+$hddfree = [math]::Round((Get-Volume -DriveLetter C).SizeRemaining /1gb)
+# IP Neu - IP from the outgoing card to 8.8.8.8
+$ip =(Test-NetConnection 8.8.8.8 -informationLevel "Detailed").SourceAddress.IPAddress
 
-# Daten für Create und Update
-$daten_update = @{ uuid="$uuid";ip="$SourceAddress";os="$os";version="$version";uptime="$uptime"}
-$daten_create = @{ hostname="$env:computername";uuid="$uuid";ip="$SourceAddress";os="$os";version="$version";uptime="$uptime"}
+# Get external IP - !!!! Use your own script and your own server here!!! -
+$externalip =(Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
 
+
+# Gateway // NextHop
+$gateway = (test-NetConnection 8.8.8.8 -informationLevel "Detailed").NetRoute.NextHop
+# Data for Create and Update
+$daten_update = @{ uuid="$uuid";ip="$ip";os="$os";version="$version";uptime="$uptime";cpuname="$cpuname";cpuload="$cpuload";ram="$ram";freemem="$freemem";logonserver="$logonserver";loginuser="$loginuser";vendor="$vendor";hardwarename="$hardwarename";biosfirmaretype="$biosfirmaretype";hdd="$hdd";hddsize="$hddsize";hddfree="$hddfree";externalip="$externalip";gateway="$gateway"}
+$daten_create = @{ hostname="$env:computername";uuid="$uuid";ip="$ip";os="$os";version="$version";uptime="$uptime";cpuname="$cpuname";cpuload="$cpuload";ram="$ram";freemem="$freemem";logonserver="$logonserver";loginuser="$loginuser";vendor="$vendor";hardwarename="$hardwarename";biosfirmaretype="$biosfirmaretype";hdd="$hdd";hddsize="$hddsize";hddfree="$hddfree";externalip="$externalip";gateway="$gateway"}
 # Update inventory
 Invoke-Restmethod -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Uri $url/$env:computername -Method Put -Body $daten_update -ContentType "application/x-www-form-urlencoded" | Select-Object -Expand message -OutVariable message
 if($message -eq "Inventory item updated successfully") {
