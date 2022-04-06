12069f76f849836b8d445d587c71c43fab67398b
Marc-Andre Bombeck <MBombeck@gmail.com>
2022-04-06 13:58:27 +0200

    fix max sql connection error

--- services/SQLService.js
+++ services/SQLService.js
@@ -4,7 +4,7 @@ const config = require('../config/config');
 async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
   const [results] = await connection.execute(sql, params);
-
+  connection.end();
   return results;
 }
 
