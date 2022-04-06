77b031b2526904676b05eb71b76c58189d85c503
Marc-Andre Bombeck <MBombeck@gmail.com>
2022-04-06 17:50:01 +0200

    with a little help from my friends 🙈

--- services/SQLService.js
+++ services/SQLService.js
@@ -1,13 +1,20 @@
 const mysql = require('mysql2/promise');
 const config = require('../config/config');
+const log4js = require('log4js');
 
 async function query(sql, params) {
   const connection = await mysql.createConnection(config.db);
-  const [results] = await connection.execute(sql, params);
-  connection.end();
-  return results;
+  try {
+    const [results] = await connection.execute(sql, params);
+    connection.end();
+    return results;
+  } catch (err) {
+    connection.end();
+    //logger.error(err); 
+   } 
 }
 
 module.exports = {
   query,
 };
+
